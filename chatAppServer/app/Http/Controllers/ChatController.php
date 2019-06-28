db8005fa0c3ccfa9969afcfc6e96fabf4b914ac6
saleh <saleh.afzoon.76@gmail.com>
2019-06-28 10:43:22 +0430

    send mess and delete member and add member api corrected

--- chatAppServer/app/Http/Controllers/ChatController.php
+++ chatAppServer/app/Http/Controllers/ChatController.php
@@ -10,137 +10,188 @@ use Auth;
 
 class ChatController extends Controller
 {
-    public function create(Request $request){
+    public function create(Request $request)
+    {
 
         $user = User::find(auth()->user()->id);
-            
+
         $chat = new Chat;
         $chat->name = $request->input('name');
         $chat->is_private = $request->input('isPrivate');
         $chat->is_channel = $request->input('isChannel');
-        
-        $chat->save();
-        
-        $others = array();
-            
-        if(count($request->input('others'))!=1){
 
-            foreach ($request->input('others') as $uid){
-            
+        $chat->save();
+
+        $others = array();
+
+        if (count($request->input('others')) != 1) {
+
+            foreach ($request->input('others') as $uid) {
+
                 $other = User::find($uid);
-                
-                if(is_null($other)){
+
+                if (is_null($other)) {
                     $chat->delete();
-                    return response()->json(['message' => 'user not found',
-                        ],200);
+                    return response()->json([
+                        'message' => 'user not found',
+                    ], 200);
                 }
-                array_push($others,$other);
+                array_push($others, $other);
             }
-            
+
             $chat->members()->save($user);
 
             DB::table('chat_user')
-            ->where('user_id',$user->id)
-            ->where('chat_id',$chat->id)
-            ->update(['permission' => "ADMIN"]);
-            
+                ->where('user_id', $user->id)
+                ->where('chat_id', $chat->id)
+                ->update(['permission' => "ADMIN"]);
+
             $chat->members()->saveMany($others);
-         
-        }else{
+        } else {
             $chat->members()->save($user);
-            $other = User::find( $request->input('others')[0] );
-            
-            if(is_null($other)){
+            $other = User::find($request->input('others')[0]);
+
+            if (is_null($other)) {
                 $chat->delete();
-                return response()->json(['message' => 'user not found',
-                    ],200);
+                return response()->json([
+                    'message' => 'user not found',
+                ], 200);
             }
 
             $chat->members()->save($other);
-            DB::table('chat_user')->where('chat_id',$chat->id)->update(['permission' => "ADMIN"]);
+            DB::table('chat_user')->where('chat_id', $chat->id)->update(['permission' => "ADMIN"]);
         }
 
         return response()->json([
             'message' => 'chat created',
         ], 200);
     }
-    public function delete(Request $request){
+    public function delete(Request $request)
+    {
 
-        $chat = Chat::find( $request->chat_id);
+        $chat = Chat::find($request->chat_id);
 
-        if(is_null($chat)){
-            return response()->json(['message' => 'chat not found',
-                ],200);
+        if (is_null($chat)) {
+            return response()->json([
+                'message' => 'chat not found',
+            ], 200);
         }
+
+        $res = DB::table('chat_user')->where('chat_id', $request->chat_id)
+            ->where('user_id', auth()->user()->id)
+            ->where('permission', 'ADMIN')
+            ->get();
+
+        if (count($res) == 0) {
+            return response()->json([
+                'message' => 'not allowed',
+            ], 403);
+        }
+        
         $chat->delete();
-    
-        return response()->json(['message' => 'chat deleted',
-        ],200);
 
+        return response()->json([
+            'message' => 'chat deleted',
+        ], 200);
     }
-    public function info(Request $request){
-        $chat = Chat::find( $request->chat_id);
+    public function info(Request $request)
+    {
+        $chat = Chat::find($request->chat_id);
 
-        if(is_null($chat)){
-            return response()->json(['message' => 'chat not found',
-                ],200);
+        if (is_null($chat)) {
+            return response()->json([
+                'message' => 'chat not found',
+            ], 200);
         }
 
-        return response()->json(['info' => $chat
-        ],200);
+        return response()->json([
+            'info' => $chat
+        ], 200);
     }
-    public function addMember(Request $request){
+    public function addMember(Request $request)
+    {
 
-        $chat = Chat::find( $request->chat_id);
+        $chat = Chat::find($request->chat_id);
         $user = User::find($request->user_id);
 
-        if(is_null($chat) or is_null($user)){
-            return response()->json(['message' => 'chat or user not found',
-                ],200);
+        if (is_null($chat) or is_null($user)) {
+            return response()->json([
+                'message' => 'chat or user not found',
+            ], 200);
         }
 
-        if($chat->members->contains($user))
-            return response()->json(['message' => 'user is already a member',
-                ],200);
-        
+        $res = DB::table('chat_user')->where('chat_id', $request->chat_id)
+            ->where('user_id', auth()->user()->id)
+            ->where('permission', 'ADMIN')
+            ->get();
+
+        if (count($res) == 0) {
+            return response()->json([
+                'message' => 'not allowed',
+            ], 403);
+        }
+
+        if ($chat->members->contains($user))
+            return response()->json([
+                'message' => 'user is already a member',
+            ], 200);
+
         $chat->members()->save($user);
 
         return response()
-        ->json(['message' => 'member added'
-        ],200);
+            ->json([
+                'message' => 'member added'
+            ], 200);
     }
 
-    public function deleteMember(Request $request){
+    public function deleteMember(Request $request)
+    {
 
-        $chat = Chat::find( $request->chat_id);
+        $chat = Chat::find($request->chat_id);
         $user = User::find($request->user_id);
 
-        if(is_null($chat) or is_null($user)){
-            return response()->json(['message' => 'chat or user not found',
-                ],200);
+        if (is_null($chat) or is_null($user)) {
+            return response()->json([
+                'message' => 'chat or user not found',
+            ], 200);
+        }
+
+        $res = DB::table('chat_user')->where('chat_id', $request->chat_id)
+            ->where('user_id', auth()->user()->id)
+            ->where('permission', 'ADMIN')
+            ->get();
+
+        if (count($res) == 0) {
+            return response()->json([
+                'message' => 'not allowed',
+            ], 403);
         }
 
         DB::table('chat_user')->where('chat_id', $chat->id)->where('user_id', $user->id)->delete();
 
         return response()
-        ->json(['message' => 'member deleted'
-        ],200);
-
+            ->json([
+                'message' => 'member deleted'
+            ], 200);
     }
 
-    public function members(Request $request){
+    public function members(Request $request)
+    {
 
         $chat = Chat::find($request->chat_id);
-        
-        if(is_null($chat))
-            return response()->json(['message' => 'not found',
-                ],200);
-       
-        if(!$chat->members->contains(auth()->user()))
-            return response()->json(['message' => 'not allowed',
-                ],403);
-        
-        return response()->json(['members' => $chat->members,
-            ],200);
+
+        if (is_null($chat))
+            return response()->json([
+                'message' => 'not found',
+            ], 200);
+
+        if (!$chat->members->contains(auth()->user()))
+            return response()->json([
+                'message' => 'not allowed',
+            ], 403);
+
+        return response()->json([
+            'members' => $chat->members,
+        ], 200);
     }
 }
--- chatAppServer/app/Http/Controllers/MessageController.php
+++ chatAppServer/app/Http/Controllers/MessageController.php
@@ -6,33 +6,54 @@ use Illuminate\Http\Request;
 use App\Message;
 use App\User;
 use App\Chat;
-
+use DB;
 
 class MessageController extends Controller
 {
-    
-    public function autherize(Request $request){
+
+    public function autherize(Request $request)
+    { }
+    public function send(Request $request)
+    {
+        $chat =Chat::find($request->chat_id);
+
+        if (is_null($chat)) {
+            return response()->json([
+                'message' => 'chat not found',
+            ], 200);
+        }
         
-    }
-    public function send(Request $request){
+        if($chat->is_private == 1 or $chat->is_channel == 1 ){
+            $res = DB::table('chat_user')->where('chat_id', $request->chat_id)
+                ->where('user_id', auth()->user()->id)
+                ->where('permission', 'ADMIN')
+                ->get();
+
+            if (count($res)==0) {
+                return response()->json([
+                    'message' => 'not allowed',
+                ], 403);
+            }
+        }
 
         $message = new Message;
         $message->content = $request->content;
         $message->chat_id = $request->chat_id;
-        
+
         $message->save();
 
         return response()->json([
             'message' => 'message sended',
+            'user_id'=> auth()->user()->id
         ], 200);
     }
-    public function chatMessages(Request $request){
+    public function chatMessages(Request $request)
+    {
 
         $chat = Chat::find($request->chat_id);
-        
+
         return response()->json([
-            'chat_messages' => $chat ->messegaes
-        ],200);
+            'chat_messages' => $chat->messegaes
+        ], 200);
     }
-    
 }
